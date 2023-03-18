40e3ca0142eb51f6b8d7da104d01c6cfa05b711f
Zafer Gürgil <116303717+Vilof@users.noreply.github.com>
2023-03-18 18:47:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Zafer GÜRGİL")
+print("March 18th 2023")
+print("I am signing uf for Replit's 100 days of Python challenge!")
+print("I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.")
+print("I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen to be. No excuses for not coding from the middle of a field!")
+print(" I am feeling 😎")
+print("You can follow my progress at replit.com/@ZaferGurgil")
